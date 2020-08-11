4cfd4c2b2cea3b9872b7324a4fed510ee7e69cf1
Zach Hembree <zhembree654@gmail.com>
2020-08-10 21:00:54 -0400

    Refactor HudMain client to support Z-sorting and updated cursor

--- Client/UI/HUD/Rendering/TextBoard.cs
+++ Client/UI/HUD/Rendering/TextBoard.cs
@@ -58,7 +58,7 @@ namespace RichHudFramework
                 /// <summary>
                 /// Scale of the text board. Applied after scaling specified in GlyphFormat.
                 /// </summary>
-                public float Scale { get { return ScaleProp.Getter(); } set { ScaleProp.Setter(value); } }
+                public float Scale { get { return GetScaleFunc(); } set { SetScaleAction(value); } }
 
                 /// <summary>
                 /// Size of the text box as rendered
@@ -82,7 +82,7 @@ namespace RichHudFramework
                 /// <summary>
                 /// Size of the text box when AutoResize is set to false. Does nothing otherwise.
                 /// </summary>
-                public Vector2 FixedSize { get { return FixedSizeProp.Getter(); } set { FixedSizeProp.Setter(value); } }
+                public Vector2 FixedSize { get { return GetFixedSizeFunc(); } set { SetFixedSizeAction(value); } }
 
                 /// <summary>
                 /// If true, the text board will automatically resize to fit the text.
@@ -102,10 +102,12 @@ namespace RichHudFramework
                     set { GetOrSetMemberFunc(value, (int)TextBoardAccessors.VertAlign); }
                 }
 
-                private readonly PropWrapper<float> ScaleProp;
+                private readonly Func<float> GetScaleFunc;
+                private readonly Action<float> SetScaleAction;
                 private readonly Func<Vector2> GetSizeFunc;
                 private readonly Func<Vector2> GetTextSizeFunc;
-                private readonly PropWrapper<Vector2> FixedSizeProp;
+                private readonly Func<Vector2> GetFixedSizeFunc;
+                private readonly Action<Vector2> SetFixedSizeAction;
                 private readonly Action<Vector2, MatrixD> DrawAction;
 
                 public TextBoard() : this(HudMain.GetTextBoardData())
@@ -114,10 +116,12 @@ namespace RichHudFramework
                 private TextBoard(TextBoardMembers members) : base(members.Item1)
                 {
                     Format = GlyphFormat.Black;
-                    ScaleProp = new PropWrapper<float>(members.Item2);
+                    GetScaleFunc = members.Item2.Item1;
+                    SetScaleAction = members.Item2.Item2;
                     GetSizeFunc = members.Item3;
                     GetTextSizeFunc = members.Item4;
-                    FixedSizeProp = new PropWrapper<Vector2>(members.Item5);
+                    GetFixedSizeFunc = members.Item5.Item1;
+                    SetFixedSizeAction = members.Item5.Item2;
                     DrawAction = members.Item6;
                 }
 
