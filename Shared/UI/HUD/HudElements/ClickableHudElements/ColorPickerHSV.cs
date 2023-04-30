2cb32013ac136e8606e95cd6cf2ee53fb645908f
Zach Hembree <zhembree654@gmail.com>
2023-04-30 19:38:08 -0400

    Fix ColorPickerHSV slider names

--- Shared/UI/HUD/HudElements/ClickableHudElements/ColorPickerHSV.cs
+++ Shared/UI/HUD/HudElements/ClickableHudElements/ColorPickerHSV.cs
@@ -15,6 +15,29 @@ namespace RichHudFramework.UI
     {
         protected override void Layout()
         {
+            _color = new Color()
+            {
+                R = (byte)Math.Round(sliders[0].Current),
+                G = (byte)Math.Round(sliders[1].Current),
+                B = (byte)Math.Round(sliders[2].Current),
+                A = 255
+            };
+
+            valueBuilder.Clear();
+            valueBuilder.Append("H: ");
+            valueBuilder.Append(_color.R);
+            sliderText[0].TextBoard.SetText(valueBuilder);
+
+            valueBuilder.Clear();
+            valueBuilder.Append("S: ");
+            valueBuilder.Append(_color.G);
+            sliderText[1].TextBoard.SetText(valueBuilder);
+
+            valueBuilder.Clear();
+            valueBuilder.Append("V: ");
+            valueBuilder.Append(_color.B);
+            sliderText[2].TextBoard.SetText(valueBuilder);
+
             display.Color = (_color / new Vector3(360f, 100f, 100f)).HSVtoColor();
         }
     }
