5554b2a6901ab6bf261bc20bbc830670e32e6b79
Zach Hembree <zhembree654@gmail.com>
2024-07-14 04:44:32 -0400

    Set RebindPage aliasing per-group

--- Client/UI/SettingsMenu/Pages/RebindPage.cs
+++ Client/UI/SettingsMenu/Pages/RebindPage.cs
@@ -35,21 +35,6 @@ namespace RichHudFramework
             public IReadOnlyList<IBindGroup> BindGroups => bindGroups;
 
             /// <summary>
-            /// If true, then the menu will allow bind alias configuration
-            /// </summary>
-            public bool IsAliased 
-            { 
-                get 
-                { 
-                    return (bool)GetOrSetMemberFunc(null, (int)RebindPageAccessors.GetOrSetIsAliased); 
-                } 
-                set
-                {
-                    GetOrSetMemberFunc(value, (int)RebindPageAccessors.GetOrSetIsAliased);
-                }
-            }
-
-            /// <summary>
             /// Allows addition of bind groups using collection-initializer syntax
             /// </summary>
             public RebindPage GroupContainer => this;
@@ -64,23 +49,25 @@ namespace RichHudFramework
             /// <summary>
             /// Adds the given bind group to the page.
             /// </summary>
-            public void Add(IBindGroup bindGroup)
+            /// <param name="isAliased">Exposes bind aliases for group if true</param>
+            public void Add(IBindGroup bindGroup, bool isAliased = false)
             {
-                GetOrSetMemberFunc(bindGroup.ID, (int)RebindPageAccessors.Add);
+                GetOrSetMemberFunc(new MyTuple<object, BindDefinitionData[], bool>(bindGroup.ID, null, isAliased), (int)RebindPageAccessors.Add);
                 bindGroups.Add(bindGroup);
             }
 
             /// <summary>
             /// Adds the given bind group to the page along with its associated default configuration.
             /// </summary>
-            public void Add(IBindGroup bindGroup, BindDefinition[] defaultBinds)
+            /// <param name="isAliased">Exposes bind aliases for group if true</param>
+            public void Add(IBindGroup bindGroup, BindDefinition[] defaultBinds, bool isAliased = false)
             {
                 BindDefinitionData[] data = new BindDefinitionData[defaultBinds.Length];
 
                 for (int n = 0; n < defaultBinds.Length; n++)
                     data[n] = (BindDefinitionData)defaultBinds[n];
 
-                GetOrSetMemberFunc(new MyTuple<object, BindDefinitionData[]>(bindGroup.ID, data), (int)RebindPageAccessors.Add);
+                GetOrSetMemberFunc(new MyTuple<object, BindDefinitionData[], bool>(bindGroup.ID, data, isAliased), (int)RebindPageAccessors.Add);
                 bindGroups.Add(bindGroup);
             }
 
